ca11c9b790e968796f6ece059ff7e528efcc4c3f
Leonhard Holmer Patoschka <e12120510@student.tuwien.ac.at>
2024-03-08 16:42:20 +0100

    mpi_send_recv

---
+++ code_examples/24_mpi_group_cart/mpi_group_cart.c
@@ -0,0 +1,20 @@
+// Page 193
+// REMARK I think that here so few of the variables are initialized
+// etc. that the algorithm can not be comfortably pasted.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <mpi.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 1)
+    {
+        printf("usage: mpi_group\n");
+        exit(1);
+    }
+
+    return 0;
+}
---
+++ code_examples/25_mpi_send_recv/mpi_send_recv.c
@@ -0,0 +1,64 @@
+// Page 193
+
+#define TAG 1000
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <mpi.h>
+#include "util.h"
+
+int main(int argc, char *argv[])
+{
+
+    if (argc != 2)
+    {
+        printf("usage: mpi_send_recv length\n");
+        exit(1);
+    }
+
+    int length = atoi(argv[1]);
+    int root = 0;
+    int rank,
+        size;
+    MPI_Comm comm = MPI_COMM_WORLD;
+
+    int count = length;
+
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_rank(comm, &rank);
+    MPI_Comm_size(comm, &size);
+
+    printf("rank: %i size: %i\n", rank, size);
+
+    assert(size > 1);
+    int *buffer = (int *)malloc(sizeof(int) * length);
+
+    for (int i = 0; i < length; i++)
+    {
+        buffer[i] = rank;
+    }
+
+    printArrayInt(length, buffer);
+
+    if (rank == root)
+    {
+        MPI_Send(buffer, count, MPI_INT, (rank + 1) % size, TAG, comm);
+    }
+    else if (rank == (root - 1 + size) % size)
+    {
+        MPI_Status status;
+        MPI_Recv(buffer, count, MPI_INT, (rank - 1 + size) % size, TAG,
+                 comm, &status);
+    }
+    else
+    {
+        MPI_Status status;
+        MPI_Recv(buffer, count, MPI_INT, (rank - 1 + size) % size, TAG,
+                 comm, &status);
+        MPI_Send(buffer, count, MPI_INT, (rank + 1) % size, TAG, comm);
+    }
+
+    printArrayInt(length, buffer);
+}
