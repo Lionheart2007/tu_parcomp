f6b17472becc38690e0e5ba3b9afec6c322dbabe
Leonhard Holmer Patoschka <e12120510@student.tuwien.ac.at>
2024-03-08 15:12:50 +0100

    mpi_cart

--- code_examples/21_mpi/mpi.c
+++ code_examples/21_mpi/mpi.c
@@ -1,3 +1,5 @@
+// Page 186
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
---
+++ code_examples/22_mpi_cart/mpi_cart.c
@@ -0,0 +1,55 @@
+// Page 183
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <mpi.h>
+
+int main(int argc, char *argv[])
+{
+
+    if (argc != 3)
+    {
+        printf("usage: mpi_cart threads dimension\n");
+        exit(1);
+    }
+
+    MPI_Comm comm;
+    int result;
+    MPI_Init(&argc, &argv);
+    comm = MPI_COMM_WORLD;
+
+    int p = atoi(argv[1]);
+    int d = atoi(argv[2]);
+
+    MPI_Comm_size(comm, &p);
+    for (d = 1; d <= p; d++)
+    {
+        int dims[d], periods[d];
+        int coords[d];
+        MPI_Comm cartcomm;
+        int rank, size;
+        int r, dd, i;
+        for (i = 0; i < d; i++)
+            dims[i] = 0;
+        MPI_Dims_create(p, d, dims);
+        for (i = 0; i < d; i++)
+            periods[i] = 0;
+        MPI_Cart_create(comm, d, dims, periods, 0, &cartcomm);
+        assert(cartcomm != MPI_COMM_NULL);
+        MPI_Comm_rank(cartcomm, &rank);
+        MPI_Cart_coords(cartcomm, rank, d, coords);
+        r = 0;
+        dd = 1;
+        for (i = d - 1; i >= 0; i--)
+        {
+            r += coords[i] * dd;
+            dd *= dims[i];
+        }
+        assert(r == rank);
+        MPI_Comm_free(&cartcomm);
+
+        return 0;
+    }
+}
